c7e557576c6ec39c0387e525bf80ce6e8fd95f56
Kanta Endo <kanta.040803@gmail.com>
2026-05-23 18:01:01 -0500

    feat: wire up cart router and finalize app entry point

--- app/database.py
+++ app/database.py
@@ -7,8 +7,9 @@ from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+
 # Setup the Database Engine
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
