0c100788865ef283fe3f908b5077bef12765abf8
Kanta Endo <kanta.040803@gmail.com>
2026-05-29 14:05:04 -0500

    feat: add Session model to app/models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.sql import func
 
 
 class Cart(Base):
@@ -8,3 +9,18 @@ class Cart(Base):
     id = Column(Integer, primary_key=True, index=True)
     cart_number = Column(String, unique=True, index=True)
     status = Column(String, default="AVAILABLE")
+
+class Session(Base):
+    __tablename__ = "sessions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    cart_id = Column(Integer, ForeignKey("carts.id"))
+    first_name = Column(String)
+    last_name = Column(String)
+    phone_number = Column(String)
+    room_number = Column(String)
+
+    # Auto-set to current timestamp when the session is created
+    checked_out_at = Column(DateTime, server_default=func.now())
+    due_at = Column(DateTime)
+    returned_at = Column(DateTime, nullable=True)
